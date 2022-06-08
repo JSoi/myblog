59f3c7aaf89f8620a38d32007409613d134e573f
JSOI <jsoiworks@gmail.com>
2022-06-08 12:42:31 +0900

    Testing CommentService in Progress

---
+++ src/test/java/com/jsoi/myblog/service/CommentServiceTest.java
@@ -0,0 +1,25 @@
+package com.jsoi.myblog.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addComment() {
+    }
+
+    @Test
+    void updateComment() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
